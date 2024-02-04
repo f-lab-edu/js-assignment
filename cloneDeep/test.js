4ca7d2c05b10cb287c39ddd6ffba5d43c2ba4980
mindong2 <qlsnaos12@naver.com>
2024-02-04 18:40:58 +0900

    테스트 코드 수정, gitignore설정

--- cloneDeep/test.js
+++ cloneDeep/test.js
@@ -38,7 +38,7 @@ const cloneDeep = (param) => {
   } else {
     copyItem = {};
     for (let key in param) {
-      if (typeof param[key] === "object" && param[key] !== null) {
+      if (isObject(param[key])) {
         copyItem[key] = cloneDeep(param[key]);
       } else {
         copyItem[key] = param[key];
@@ -66,34 +66,51 @@ test("cloneDeep: Boolean", () => {
 
 const obj = { a: 1, b: 2, c: { d: 3, e: 5 } };
 
+// 두 객체의 프로퍼티값 비교
+
 test("cloneDeep: Object", () => {
+  expect(cloneDeep(obj)).toStrictEqual(obj);
+});
+
+// 깊은복사 여부
+test("cloneDeep: deep copy of Object", () => {
   expect(cloneDeep(obj) === obj).toBe(false);
 });
 
 // 2depth이상의 객체에서 깊은복사 여부
-test("cloneDeep: Object", () => {
+test("cloneDeep: 2depth deep copy of Object", () => {
   expect(cloneDeep(obj).c === obj.c).toBe(false);
 });
 
 const arr = [3, { a: "b", c: "d" }, 1];
 
 test("cloneDeep: Array", () => {
+  expect(cloneDeep(arr)).toStrictEqual(arr);
+});
+
+// 깊은복사 여부
+test("cloneDeep: deep copy of Array", () => {
   expect(cloneDeep(arr) === arr).toBe(false);
 });
 
 // 2depth이상의 배열에서 깊은복사 여부
-test("cloneDeep: Array", () => {
+test("cloneDeep: 2depth deep copy of Array", () => {
   expect(cloneDeep(arr)[1] === arr[1]).toBe(false);
 });
 
 const set = new Set([1, { a: 4, b: 5 }, 3, null]);
 
 test("cloneDeep: Set", () => {
+  expect(cloneDeep(set)).toStrictEqual(set);
+});
+
+// 깊은복사 여부
+test("cloneDeep: deep copy of Set", () => {
   expect(cloneDeep(set) === set).toBe(false);
 });
 
 // 2depth이상의 Set객체에서 깊은복사 여부
-test("cloneDeep: Set", () => {
+test("cloneDeep: 2depth deep copy of Set", () => {
   expect([...cloneDeep(set)][1] === [...set][1]).toBe(false);
 });
 
@@ -102,16 +119,26 @@ map.set(0, "a");
 map.set(1, { a: "b", b: "c", set: set });
 
 test("cloneDeep: Map", () => {
+  expect(cloneDeep(map)).toStrictEqual(map);
+});
+
+// 깊은복사 여부
+test("cloneDeep: deep copy of Map", () => {
   expect(cloneDeep(map) === map).toBe(false);
 });
 
 // 2depth이상의 Map객체에서 깊은복사 여부
-test("cloneDeep: Map", () => {
+test("cloneDeep: 2depth deep copy of Map", () => {
   expect([...cloneDeep(map).values()][1] === [...map.values()][1]).toBe(false);
 });
 
 const date = new Date();
 
 test("cloneDeep: Date", () => {
+  expect(cloneDeep(date)).toStrictEqual(date);
+});
+
+// 깊은복사 여부
+test("cloneDeep: deep copy of Date", () => {
   expect(cloneDeep(date) === date).toBe(false);
 });
